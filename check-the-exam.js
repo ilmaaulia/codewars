ce841310d703b7dacce308abee446634aae642cc
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2024-02-18 08:33:17 +0700

    Add solution

---
+++ check-the-exam.js
@@ -0,0 +1,11 @@
+const checkExam = (array1, array2) => {
+	let score = 0;
+
+	for (let i = 0; i < array1.length; i++) {
+		if (array2[i] === '') score += 0;
+		else if (array2[i] === array1[i]) score += 4;
+		else score -= 1;
+	}
+
+	return Math.max(score, 0);
+}
